65382720b14c57c80fa9b740952cf81fe62a03e7
kd <karna.dalal@gmail.com>
2018-07-20 20:53:09 +0200

    Signed-off-by: kd <karna.dalal@gmail.com>

--- get/get.udt.list.php
+++ get/get.udt.list.php
@@ -5,7 +5,7 @@
 
     include_once('./../cl.dbDriver.php');
     include_once('./../defs/cl.UDT.php');
-
+// 
     $DirPath_ROOT_LibUDT = (PATH__DATA_ROOT . DIRNAME__LIBUDT_ROOT);
 
     include_once('./../drivers/drv.libUDT.php');
@@ -18,7 +18,7 @@ MakeDir($DirPath_ROOT_LibUDT);
 
     $drv = new drvLibUDT();
     echo $drv->getUDTList();
-    echo "xx";
+    //echo "xx";
     exit;
 
 
